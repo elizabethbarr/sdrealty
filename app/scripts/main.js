95ac0e9049e269ea92ff0546fbba3e1954f976f9
Elizabeth Barr <elizbarr@gmail.com>
2014-09-17 13:30:43 -0400

    testimonial carousel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,8 @@
-$(document).ready(function(){
-	// Set the interval to be 5 seconds
-	var t = setInterval(function(){
-		$("#carousel ul").animate({marginLeft:-480},1000,function(){
-			$(this).find("li:last").after($(this).find("li:first"));
-			$(this).css({marginLeft:0});
-		})
-	},5000);
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  slide: '> div',
+  cssEase: 'linear'
 });
