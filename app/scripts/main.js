6f821530fc277698c8159a72d4a4a676e007478d
Elizabeth Barr <elizbarr@gmail.com>
2014-09-23 22:30:58 -0400

    jQuery for slideshow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,10 @@
-$('.fade').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  slide: '> div',
-  cssEase: 'linear'
-});
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
